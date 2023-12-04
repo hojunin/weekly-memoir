dde9f5e1f2fe903f2f5397691c4661bcec735c55
hojunin <dlsghwns@naver.com>
2023-12-05 00:22:26 +0900

    feat: memoir 생성 추rk

--- src/app/api/memoir/route.ts
+++ src/app/api/memoir/route.ts
@@ -3,24 +3,21 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const userId = searchParams.get('userId');
-
-  if (!userId) {
-    return NextResponse.json(
-      { message: '유저 아이디가 존재하지 않아요' },
-      { status: 500 },
-    );
-  }
+  const memoirId = searchParams.get('id');
 
   try {
-    const memoirData = await sql`
-        SELECT m.*, u.username as user_username
-        FROM Memoir m
-        INNER JOIN User u ON m.user = u.id
-        WHERE m.user = ${userId};
+    const result = await sql`
+        SELECT Memoir.id, Memoir.title, Memoir.description, jsonb_build_object(
+          'id', public.User.id,
+          'user_name', public.User.user_name,
+          'email', public.User.email
+      ) AS owner
+        FROM Memoir
+        JOIN public.User ON Memoir.owner = public.User.id
+        WHERE Memoir.id = ${memoirId};
       `;
 
-    return NextResponse.json({ memoirData }, { status: 200 });
+    return NextResponse.json({ result: result.rows[0] }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
@@ -28,10 +25,11 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
   const body = await request.json();
-  console.log('🚀 ~ file: route.ts:6 ~ POST ~ body:', body);
 
   try {
-    // await sql`INSERT INTO Memoir (Name, Owner) VALUES (${petName}, ${ownerName});`;
+    await sql`INSERT INTO Memoir (
+      title, description, year_week, type, owner) 
+      VALUES (${body.title}, ${body.description}, ${body.year_month}, ${body.type} , ${body.user_id});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
--- src/app/api/table/memoir/route.ts
+++ src/app/api/table/memoir/route.ts
@@ -3,10 +3,11 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   try {
-    const result = await sql`CREATE TABLE IF NOT EXISTS Memoir(
-        title varchar(255),
-        description varchar(255) 
-        owner INTEGER REFERENCES User(id),
+    const result = await sql`CREATE TABLE IF NOT EXISTS public.Memoir(
+          id SERIAL PRIMARY KEY,
+          title varchar(255),
+          description varchar(255),
+          owner INTEGER REFERENCES public.User(id)
         );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
