2b132c919c4715caded30b41d61404b4fd9def56
hojunin <dlsghwns@naver.com>
2023-12-06 17:51:42 +0900

    feat: 랜딩페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import LadingTemplate from '@/components/landing';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-y-36">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-10 text-center">
+        THE WEEKLY로 바뀌는 일주일
+      </h1>
+
       <LadingTemplate
-        title="1주일에 딱 1번, 돌아보고 기록해요."
+        title="일주일에 딱 1번, 돌아보고 기록해요."
         description="다양한 주제에 대해 한 주를 회고 할 수 있어요"
         image="https://i.imgur.com/rwipSiP.png"
         index={0}
@@ -16,8 +20,8 @@ export default function Home() {
         index={1}
       />
       <LadingTemplate
-        title="다른 사람들의 회고를 공유하고 공유받아요."
-        description="동기부여의 수단으로 삼아요"
+        title="내 일주일을 소개해요"
+        description="다른 사람들의 회고를 공유하고 공유받으며 배워요"
         image="https://i.imgur.com/dJa9nIm.png"
         index={2}
       />
