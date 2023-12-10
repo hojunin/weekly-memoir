fdc043128b69c3495bdbc2a43e8d92f4413e51b0
hojunin <dlsghwns@naver.com>
2023-12-10 12:46:02 +0900

    feat: vercel/analytics 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import Layout from '@/components/common/layout';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           <Layout>{children}</Layout>
           <Toaster />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
