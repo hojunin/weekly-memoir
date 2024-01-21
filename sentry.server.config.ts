6d7449515dd52c2f2af3915e2f76d456d7a954be
hojunin <dlsghwns@naver.com>
2024-01-21 16:26:29 +0900

    Sentry 처리

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://eae330b6dda4477c0d976b95a827beb4@o4506608043687936.ingest.sentry.io/4506608045326336",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
